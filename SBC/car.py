8e2dd0c4e69051dd487f87cfc4ec380b5fcf168a
Dsaoro <diegoalej123@gmail.com>
2023-11-09 23:46:51 -0500

    v1.0 stable - coupling between flask api and joystick js code, added bussines logic for motor control

--- SBC/car.py
+++ SBC/car.py
@@ -1,204 +1,116 @@
+from operator import le
 import os
 import sys
 from time import sleep
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, url_for, redirect,request,jsonify
 import RPi.GPIO as gpio
+# from camera import VideoCamera
 
-# class Car:
+try:
+
+    # pi_camera = VideoCamera(flip=False) # flip pi camera if upside down.
+
+    gpio.setmode(gpio.BOARD)
+    gpio.setwarnings(False)
+
+    leftMotorC = 33
+    rightMotorC = 35
+    forwardMotorP = 29
+    backwardMotorP = 31
+
+    gpio.setup(leftMotorC, gpio.OUT)
+    gpio.setup(rightMotorC, gpio.OUT)
+    gpio.setup(forwardMotorP, gpio.OUT)
+    gpio.setup(backwardMotorP, gpio.OUT)
+
+    def Left():
+        gpio.output(leftMotorC,gpio.HIGH)
+        gpio.output(rightMotorC,gpio.LOW)
+
+
+    def Right():
+        gpio.output(leftMotorC,gpio.LOW)
+        gpio.output(rightMotorC,gpio.HIGH)
+
+    def Forward():
+        gpio.output(forwardMotorP,gpio.HIGH)
+        gpio.output(backwardMotorP,gpio.LOW)
+
+    def Backward():
+        gpio.output(forwardMotorP,gpio.LOW)
+        gpio.output(backwardMotorP,gpio.HIGH)
+
+    def Stop():
+        gpio.output(forwardMotorP,gpio.LOW)
+        gpio.output(backwardMotorP,gpio.LOW)
+        gpio.output(leftMotorC,gpio.LOW)
+        gpio.output(rightMotorC,gpio.LOW)
+
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    @app.route('/left/', methods=['POST'])
+    def leftMovement():
+        Left()
+        sleep(2)
+        Stop()
+        return render_template('index.html')
+
+    @app.route('/right/', methods=['POST'])
+    def rigthMovement():
+        Right()
+        sleep(2)
+        Stop()
+        return render_template('index.html')
+
+    @app.route('/forward/', methods=['POST'])
+    def forwardMovement():
+        Forward()
+        sleep(2)
+        Stop()
+        return render_template('index.html')
+
+    @app.route('/backward/', methods=['POST'])
+    def backwardMovement():
+        Backward()
+        sleep(2)
+        Stop()
+        return render_template('index.html')
     
-#     BLINK_TIME = 0.2
-#     SAMPLE_TIME = 0.2
+    @app.route('/joystick', methods=['POST'])
+    def recibir_datos_joystick():
+        x_pos = int(request.form.get('xPos'))
+        y_pos = int(request.form.get('yPos'))
 
-#     def __init__(self,frMotorA,frMotorB,lrMotorA,lrMotorB,leftLed,rigthLed):
-        
-#         gpio.setmode(gpio.BOARD)
-#         self.motor = motor(frMotorA,frMotorB)
-#         self.direccion = motor(lrMotorA,lrMotorB)
-#         self.leftLed = leftLed
-#         self.rigthLed = rigthLed 
-#         gpio.setcfg(self.leftLed, gpio.gpio.OUTPUT)
-#         gpio.setcfg(self.rigthLed, gpio.gpio.OUTPUT)
+        if(y_pos > 50):
+            Backward()
+        if(y_pos < -50):
+            Forward()
+        if(x_pos > 50):
+            Right()
+        if(x_pos < -50):
+            Left()
+        if(y_pos > -50 and y_pos < 50):
+            gpio.output(forwardMotorP,gpio.LOW)
+            gpio.output(backwardMotorP,gpio.LOW)
+        if(x_pos > -50 and x_pos < 50):
+            gpio.output(leftMotorC,gpio.LOW)
+            gpio.output(rightMotorC,gpio.LOW)
 
-        
-#     def forward(self,speed,direction):
-#         if(direction<125):
-#             print("forward <")
-#             self.direccion.Backward()
-#             sleep(self.SAMPLE_TIME)
-#             self.direccion.stop()
-#         elif(direction>125):
-#             print("forward >")
-#             self.direccion.forward()
-#             sleep(self.SAMPLE_TIME)
-#             self.direccion.stop()
-#         if(speed >125):
-#             self.motor.forward()
-#             sleep(self.SAMPLE_TIME)
-#             self.motor.stop()
-#         print("forward")
+        return jsonify({"mensaje": "Datos recibidos correctamente"})
 
-#     def reverse(self,speed, direction):
-#         if(direction<125):
-#             print("reverse <")
-#             self.direccion.Backward()
-#             sleep(self.SAMPLE_TIME)
-#             self.direccion.stop()
-#         elif(direction>125):
-#             print("reverse >")
-#             self.direccion.forward()
-#             sleep(self.SAMPLE_TIME)
-#             self.direccion.stop()
-#         if(speed >125):
-#             self.motor.backward()
-#             sleep(self.SAMPLE_TIME)
-#             self.motor.stop()
-#         print("reverse")
+    if __name__ == '__main__':
+        app.run(debug=True, host='0.0.0.0', threaded=True)
 
-#     def blinkL(self,bTime):
-#         print("<")
-#         gpio.gpio.output(self.leftLed,1)
-#         sleep(self.BLINK_TIME+bTime)
-#         gpio.gpio.output(self.leftLed,0)
-
-#     def blinkR(self,bTime):
-#         print(">")
-#         gpio.gpio.output(self.rigthLed,1)
-#         sleep(self.BLINK_TIME+bTime)
-#         gpio.gpio.output(self.rigthLed,0)
-
-#     def blinkStop(self,bTime):
-#         print("<   >")
-#         gpio.gpio.output(self.leftLed,1)
-#         gpio.gpio.output(self.rigthLed,1)
-#         sleep(self.BLINK_TIME+bTime)
-#         gpio.gpio.output(self.leftLed,0)
-#         gpio.gpio.output(self.rigthLed,0)
-    
-#     def shutdown(self,bTime):
-#         print("the car is shuting down")
-#         gpio.cleanup();
-
-# class motor:
-    
-#     SAMPLE_TIME=0.2
-
-#     def __init__(self, ina, inb):
-#         self.ina
-#         self.inb
-#         gpio.setup(ina, gpio.gpio.OUT)
-#         gpio.setup(inb, gpio.gpio.OUT)
-
-#     def forward(self):
-#         gpio.gpio.output(self.ina, 1)
-#         gpio.gpio.output(self.inb, 0)
-
-#     def Backward(self):
-#         gpio.gpio.output(self.ina, 0)
-#         gpio.gpio.output(self.inb, 1)
-
-#     def forwardSpeed(self,speed):
-#         gpio.gpio.output(self.ina, 1)
-#         gpio.gpio.output(self.inb, 0)
-
-#     def BackwardSpeed(self,speed):
-#         gpio.gpio.output(self.ina, 0)
-#         gpio.gpio.output(self.inb, 1)
-
-#     def stop(self):
-#         gpio.gpio.output(self.ina, 0)
-#         gpio.gpio.output(self.inb, 0)
-
-gpio.setmode(gpio, gpio.BOARD)
-
-leftMotorC = 1
-rightMotorC = 2
-fowardMotorP = 3
-backwardMotorP = 4
-
-dutyCycle = 50
-
-Stop = 0
-
-gpio.setup(leftMotorC, gpio.OUT)
-gpio.setup(rightMotorC, gpio.OUT)
-gpio.setup(fowardMotorP, gpio.OUT)
-gpio.setup(backwardMotorP, gpio.OUT)
-
-pwmLeftMotorC = gpio.PWM(leftMotorC, 100)
-pwmRightMotorC = gpio.PWM(rightMotorC, 100)
-pwmFowardMotorP = gpio.PWM(fowardMotorP, 100)
-pwmBackwardMotorP = gpio.PWM(backwardMotorP, 100)
-
-
-def Left():
-    pwmLeftMotorC.ChangeDutyCycle(50) #50% ciclo de trabajo giro izquierda
-    pwmRightMotorC.ChangeDutyCycle(0)
-    pwmFowardMotorP.ChangeDutyCycle(0)
-    pwmBackwardMotorP.ChangeDutyCycle(0)
-
-def Right():
-    pwmLeftMotorC.ChangeDutyCycle(0) 
-    pwmRightMotorC.ChangeDutyCycle(50) #50% giro derecha
-    pwmFowardMotorP.ChangeDutyCycle(0)
-    pwmBackwardMotorP.ChangeDutyCycle(0)
-
-def Foward():
-    pwmLeftMotorC.ChangeDutyCycle(0) 
-    pwmRightMotorC.ChangeDutyCycle(0)
-    pwmFowardMotorP.ChangeDutyCycle(50) #adelante
-    pwmBackwardMotorP.ChangeDutyCycle(0)
-
-def Backward():
-    pwmLeftMotorC.ChangeDutyCycle(0) 
-    pwmRightMotorC.ChangeDutyCycle(0)
-    pwmFowardMotorP.ChangeDutyCycle(0)
-    pwmBackwardMotorP.ChangeDutyCycle(50) #atrás
-
-def Stop():
-    pwmLeftMotorC.ChangeDutyCycle(0) 
-    pwmRightMotorC.ChangeDutyCycle(0)
-    pwmFowardMotorP.ChangeDutyCycle(0)
-    pwmBackwardMotorP.ChangeDutyCycle(0)
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/left/', methods=['POST'])
-def backwardMovement():
-    Left()
-    sleep(2)
-    Stop()
-    return render_template('index.html')
-
-@app.route('/right/', methods=['POST'])
-def backwardMovement():
-    Right()
-    sleep(2)
-    Stop()
-    return render_template('index.html')
-
-@app.route('/foward/', methods=['POST'])
-def fowardMovement():
-    Foward()
-    sleep(2)
-    Stop()
-    return render_template('index.html')
-
-@app.route('/backward/', methods=['POST'])
-def backwardMovement():
-    Backward()
-    sleep(2)
-    Stop()
-    return render_template('index.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', threaded=True)
-
-
+except EOFError:
+    gpio.cleanup()
+except KeyboardInterrupt:
+    gpio.cleanup()
+finally:
+    gpio.cleanup()
 
 
 
